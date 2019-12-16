b15ba79bc0cbf6b9133aa403e7a581916fafa0ee
mikelpq <mikelmpq@gmail.com>
2019-12-16 13:59:22 +0100

    FEAT: getter & setter location. TODO: userHistoryLocation

--- src/main/java/DataModel/DataModel.java
+++ src/main/java/DataModel/DataModel.java
@@ -4,9 +4,19 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
+import DataModel.Location;
+
 
 public class DataModel {
     @SerializedName("locations")
     @Expose
     private ArrayList<Location> locations;
+
+    public ArrayList<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
 }
--- src/main/java/DataModel/User.java
+++ src/main/java/DataModel/User.java
@@ -1,5 +1,7 @@
 package DataModel;
 
+import java.util.Scanner;
+
 public class User {
     private String name;
     private String mail;
@@ -9,5 +11,31 @@ public class User {
         this.name = name;
         this.mail = mail;
         this.year = year;
+    }
+
+    public void userHistoryLocations(DataModel dataModel){
+        boolean stop = false;
+        Scanner sc = new Scanner(System.in);
+        String opcio;
+
+        while (!stop){
+            System.out.println("Vols crear una nova localització?");
+            if ((opcio = sc.nextLine()).toLowerCase().equals("si") || (opcio = sc.nextLine()).toLowerCase().equals("no")){
+                stop = true;
+            }else{
+                System.out.println("Error! S'ha d'introduir si o no");
+            }
+        }
+
+        stop = false;
+
+        while(!stop){
+            for (int i = 0; i < dataModel; i++) {
+
+            }
+        }
+
+
+
     }
 }
